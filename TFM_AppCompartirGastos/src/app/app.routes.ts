190034985245cddfac6ce9b6ea2fb3154db878b6
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-05-07 11:12:38 +0200

    Creación estructura básica de Angular
    Creado nuevo proyecto. Añadidas rutas y  páginas error404, home, login, register. Creado el interfaz  IUserInterface para usuarios

---
+++ TFM_AppCompartirGastos/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { Error404Component } from './pages/error404/error404.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: Error404Component },
+];
---
+++ TFM_AppCompartirGastos/src/app/interfaces/iuser-interface.interface.ts
@@ -0,0 +1,9 @@
+export interface IUserInterface {
+    id: number;
+    email: string;
+    password: string;
+    name: string;
+    surname: string;
+    username: string;
+    telephone: number
+}
