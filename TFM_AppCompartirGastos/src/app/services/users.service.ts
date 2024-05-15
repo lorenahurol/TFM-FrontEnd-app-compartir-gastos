b09c2a317fa1c46996db50eb26dbc9f25314b28d
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-05-15 09:31:47 +0200

    Servicios
    Se añaden los servicios para usuarios, mensajes, grupos y gastos

---
+++ TFM_AppCompartirGastos/src/app/services/users.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private httpClient = inject (HttpClient)
+
+  constructor() { }
+}
