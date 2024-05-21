4754328c87552f57fdcf62f0966daa12c6b86909
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-05-21 17:23:47 +0200

    Validadores de Formulario de registro completados
    Solo validadores del front

--- TFM_AppCompartirGastos/src/app/components/register-form/registerform.component.ts
+++ TFM_AppCompartirGastos/src/app/components/register-form/registerform.component.ts
@@ -1,53 +1,69 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registerform',
+  selector: 'app-register-form',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './registerform.component.html',
-  styleUrl: './registerform.component.css'
+  styleUrl: './registerform.component.css',
 })
 export class RegisterFormComponent {
 
-  inputForm : FormGroup
+  inputForm: FormGroup;
 
   constructor() {
-    this.inputForm = new FormGroup({
-      first_name: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.pattern(/^[[:alpha:]]$/),
-      ]),
-      surname: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(2),
-        Validators.pattern(/^[[:alpha:]]|([[:alpha:]]+\s+[[:alpha:]])$/),
-      ]),
-      username: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(5),
-        Validators.pattern(/^[[:word:]]$/),
-      ]),
-      country_code: new FormControl(null, [Validators.required]),
-      telephone: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(8),
-        Validators.pattern(/^[[:digit:]]$/),
-      ]),
-      email: new FormControl(null, [
-        Validators.required,
-        Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/),
-      ]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/),
-      ]),
-    });
+    this.inputForm = new FormGroup(
+      {
+        first_name: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^\b[A-Za-z]{2,}(?:\s+[A-Za-z]{2,})*$/),
+        ]),
+        surname: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^\b[A-Za-z]{2,}(?:\s+[A-Za-z]{2,})*$/),
+        ]),
+        username: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^(?!.*\s)[a-zA-Z0-9\S]{4,}$/),
+        ]),
+        country_code: new FormControl(null, []),
+        telephone: new FormControl(null, []),
+        email: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/),
+        ]),
+        password: new FormControl(null, [Validators.required]),
+        password_confirm: new FormControl(null, [Validators.required]),
+      },
+      [this.passwordControl]
+    );
   }
 
   getDataForm(): void {
-    
-  
+    console.log(this.inputForm.value);
   }
+
+  // Función para la comprobación de todos los validadores
+  checkControl(formControlName: string, validatorName: string): boolean | undefined {
+    return (
+      this.inputForm.get(formControlName)?.hasError(validatorName) &&
+      this.inputForm.get(formControlName)?.touched
+    );
+  }
+
+  // Función para verificar que las las dos contraseñas coinciden
+  passwordControl(formValue: AbstractControl) : {passwordControl: boolean}|null {
+    const password = formValue.get('password')?.value;
+    const passwordConfirm = formValue.get('password_confirm')?.value;
+
+    if (
+      password !== passwordConfirm
+    ) {
+      return { passwordControl: true };
+    } else {
+      return null;
+    }
+  }
+
 }
