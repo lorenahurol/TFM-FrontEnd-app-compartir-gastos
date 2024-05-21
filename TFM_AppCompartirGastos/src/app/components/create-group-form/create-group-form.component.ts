c676682da7880e6effc202194beda4d7afab04cb
Lorena Hurtado <hilorena.contact@gmail.com>
2024-05-21 17:16:00 +0100

    Creacion del formulario de creacion de grupo

--- TFM_AppCompartirGastos/src/app/components/create-group-form/create-group-form.component.ts
+++ TFM_AppCompartirGastos/src/app/components/create-group-form/create-group-form.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-group-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-group-form.component.html',
   styleUrl: './create-group-form.component.css'
 })
 export class CreateGroupFormComponent {
+  expenseGroupForm: FormGroup;
 
+  // Inicializar el formulario:
+  constructor() {
+    this.expenseGroupForm = new FormGroup({
+      title: new FormControl(null, []),
+      description: new FormControl(null, []),
+      category: new FormControl(null, [])
+    }, [])
+  }
+
+  getDataForm() {
+    console.log(this.expenseGroupForm.value);
+
+  }
 }
