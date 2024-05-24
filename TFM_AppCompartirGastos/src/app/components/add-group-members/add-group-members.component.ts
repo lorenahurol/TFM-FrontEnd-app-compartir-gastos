635b43ef52c00453e820eb488896296ec266fcb6
Lorena Hurtado <hilorena.contact@gmail.com>
2024-05-24 14:38:38 +0100

    Iniciado formulario de invitacion al grupo

--- TFM_AppCompartirGastos/src/app/components/add-group-members/add-group-members.component.ts
+++ TFM_AppCompartirGastos/src/app/components/add-group-members/add-group-members.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-group-members',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-group-members.component.html',
   styleUrl: './add-group-members.component.css'
 })
 export class AddGroupMembersComponent {
 
+  inviteMembersForm: FormGroup;
+
+  constructor() {
+    this.inviteMembersForm = new FormGroup({
+      email: new FormControl("", [
+        // Validadores:
+        Validators.required,
+        Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$") // Email válido
+        // ** Comprobar que existe en BD ** //
+      ]),
+      message: new FormControl("", [
+        Validators.maxLength(400)
+      ]),
+    }, [])
+  }
+
+  getDataForm(): void {
+    console.log(this.inviteMembersForm.value);
+  }
+
+  // Comprobar validadores:
+  checkControl(formControlName: string, validatorName: string): boolean | undefined {
+    return (
+      this.inviteMembersForm.get(formControlName)?.hasError(validatorName) &&
+      this.inviteMembersForm.get(formControlName)?.touched
+    );
+  }
 }
