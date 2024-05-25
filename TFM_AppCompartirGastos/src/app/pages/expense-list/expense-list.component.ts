8c1674e3c709332d4668333416599d0458c21f90
emiliojcheca <emilioj.checa@gmail.com>
2024-05-25 17:34:23 +0200

    Primera versión del listado de gastos

--- TFM_AppCompartirGastos/src/app/pages/expense-list/expense-list.component.ts
+++ TFM_AppCompartirGastos/src/app/pages/expense-list/expense-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IExpense } from '../../interfaces/iexpense.interface';
+import { ExpensesService } from '../../services/expenses.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-expense-list',
@@ -8,5 +12,33 @@ import { Component } from '@angular/core';
   styleUrl: './expense-list.component.css'
 })
 export class ExpenseListComponent {
+  arrExpenses: IExpense[] = [];
+  groupId: string = '';
+  expenseService = inject(ExpensesService);
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(async (params: any) => {
+      if (params.groupId) {
+        this.groupId = params.groupId;
+        try {
+          this.arrExpenses = await this.expenseService.getExpensesByGroup(params.groupId);
+          console.log(this.arrExpenses);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
+  }
+
+  editExpense(expenseId: number) {
+    console.log(expenseId);
+    this.router.navigate([`/home/expenses/${this.groupId}/edit/${expenseId}`]);
+  }
+
+  formatDate(date: Date): string {
+    return dayjs(date).format('DD/MM/YYYY');
+  }
 
 }
