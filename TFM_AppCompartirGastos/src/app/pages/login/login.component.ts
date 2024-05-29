a96f5ecdf83f4efcc71e5fc3c08cb5b2ff94e600
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-05-29 07:27:16 +0200

    Añadidos comentarios al código

--- TFM_AppCompartirGastos/src/app/pages/login/login.component.ts
+++ TFM_AppCompartirGastos/src/app/pages/login/login.component.ts
@@ -11,31 +11,48 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  authServices = inject(AuthService);
+  router = inject(Router);
 
-  authServices = inject(AuthService)
-  router = inject (Router)
-
+  /**
+   * Handles form submission for login.
+   *
+   * @param loginForm {FormGroup} - The login form object.
+   * @returns {Promise<void>} - A promise that resolves after login attempt.
+   *
+   */
   async getDataForm(loginForm: any) {
-    let loginBody = loginForm.value
+    let loginBody = loginForm.value;
     if (!loginBody.mail || !loginBody.password) {
-      alert("Los campos email y contraseña son obligatorios")
+      alert('Los campos email y contraseña son obligatorios');
     } else {
-      
-      // configura manualmente el valor de rememberMe en caso no haya sido tocado por el usuario
-      if (loginBody.rememberMe === "")
-        loginBody.rememberMe = false
+      // configura manualmente el valor de rememberMe en caso no haya sido tocado por el usuario (por defecto html devuelve un campo vacío en vez de false)
+      if (loginBody.rememberMe === '') loginBody.rememberMe = false;
+      // recibe el token de login y lo guarda en local storage
       try {
-        let response = await this.authServices.login(loginBody)
-        localStorage.setItem('login_token', response.token!)
-        this.router.navigateByUrl ('/home')
+        let response = await this.authServices.login(loginBody);
+        localStorage.setItem('login_token', response.token!);
+        this.router.navigateByUrl('/home');
       } catch (err: any) {
-        alert (err.error.error)
+        alert(err.error.error);
       }
     }
   }
 
-  ngOnInit() {
-    if (localStorage.getItem('login_token')) this.router.navigateByUrl('/home')
+  /**
+   * Checks for existing login token on component initialization.
+   *
+   * @returns {Promise<void>} - A promise that resolves after token verification (if any).
+   */
+  async ngOnInit() {
+    const token = localStorage.getItem('login_token');
+    if (token) {
+      try {
+        const response = await this.authServices.verifyToken(token);
+        if (!response.error) this.router.navigateByUrl('/home');
+      } catch (error) {
+        alert(error);
+      }
+    }
   }
-
 }
