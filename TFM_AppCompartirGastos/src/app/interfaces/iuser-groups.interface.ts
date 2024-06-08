cab07606ac9119d43ff1c84ba90d6a9cfff3d5e4
emiliojcheca <emilioj.checa@gmail.com>
2024-06-08 11:17:39 +0200

    primera versión de home de usuario

---
+++ TFM_AppCompartirGastos/src/app/interfaces/iuser-groups.interface.ts
@@ -0,0 +1,6 @@
+export interface IUserGroups {
+    group_id:    number;
+    description: string;
+    category:    string;
+    is_admin?:   boolean;
+}
--- TFM_AppCompartirGastos/src/app/pages/user-dashboard/user-dashboard.component.ts
+++ TFM_AppCompartirGastos/src/app/pages/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IUserGroups } from '../../interfaces/iuser-groups.interface';
+import { GroupsService } from '../../services/groups.service';
+import { IRoles } from '../../interfaces/iroles.interface';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
   templateUrl: './user-dashboard.component.html',
   styleUrl: './user-dashboard.component.css',
 })
-export class UserDashboardComponent {}
+export class UserDashboardComponent {
+  arrInfoGroups: IUserGroups[] = [];
+  roles: IRoles | any = {};
+  groupService = inject(GroupsService);
+  
+
+  async ngOnInit() {
+    await this.groupService.getAllInfoGroupsByUser().then((data: IUserGroups[]) => {
+      this.arrInfoGroups = data;
+    });
+
+    this.roles = await this.groupService.getUserRolesByGroup();
+
+    /* recorrer arrInfoGroups y añadimos isAdmin a true si el group_id está en this.roles.admingroups */
+    this.arrInfoGroups.forEach((group: IUserGroups) => {
+      group.is_admin = this.roles.admingroups.includes(group.group_id);
+    });
+
+  }
+}
