3fc4276f9d90e46374bb3685fc1c3bbdede54cf0
emiliojcheca <emilioj.checa@gmail.com>
2024-06-10 14:13:34 +0200

    recuperar el usuario creador y mejorar el aspecto

--- TFM_AppCompartirGastos/src/app/interfaces/iuser-groups.interface.ts
+++ TFM_AppCompartirGastos/src/app/interfaces/iuser-groups.interface.ts
@@ -2,6 +2,7 @@ export interface IUserGroups {
     group_id:    number;
     description: string;
     category:    string;
+    creator_user: string;
     is_admin?:   boolean;
     totalexpenses?: number;
     balance?: number;
