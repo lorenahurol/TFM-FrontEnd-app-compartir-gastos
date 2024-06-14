a32686a6c14e51aaac8a701893870a813740271f
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-06-14 10:29:31 +0200

    Eliminadas importaciones obsoletas y añadido el modal al authguard

--- TFM_AppCompartirGastos/src/app/common/guards/auth.guard.ts
+++ TFM_AppCompartirGastos/src/app/common/guards/auth.guard.ts
@@ -1,11 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { AlertModalService } from '../../services/alert-modal.service';
 
 export const authGuard: CanActivateFn = async (route, state) => {
   const router = inject(Router);
   const authServices = inject (AuthService)
   const token = localStorage.getItem('login_token')
+  const alertModalService = inject(AlertModalService)
 
   // comprueba que hay token en local storage y lo verifica en la API
   if (token) {
@@ -13,8 +15,20 @@ export const authGuard: CanActivateFn = async (route, state) => {
     if (response.error) return false
     return true
   } else {
-    alert ('Necesitas estar autenticado')
-    router.navigateByUrl('/login')
+      const alertModal = alertModalService.newAlertModal({
+        icon: 'warning',
+        title: 'Acceso denegado',
+        body: 'Para acceder necesitas estar autenticado. Te llevamos al login',
+        acceptAction: true,
+        backAction: false,
+      });
+      alertModal?.componentInstance.sendModalAccept.subscribe(
+        (isAccepted) => {
+          if (isAccepted) {
+            router.navigateByUrl('/login');
+          }
+        }
+      );
     return false;
   }
 };
